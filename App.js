ced990ee966996950b4436e8ba231f8676f8aa6c
Prabhat Solanki <155136172+prabhats-github@users.noreply.github.com>
2024-02-02 14:19:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Setstate01 from './componants/Setstate01'
+
+function App() {
+  return (
+    <div className="App">
+      <Setstate01/>
+    </div>
+  );
+}
+
+export default App;
